16c698a05b053d30801733dec13c007ad44dc8c1
maiya777 <maiya777@list.ru>
2015-10-24 20:17:33 +0300

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each {|attribute, value|
+    value.each {|key, names|
+      names.each { |name| if pigeon_list.has_key?(name)
+        if pigeon_list[name].has_key?(attribute)
+          pigeon_list[name][attribute] << key.to_s
+        else pigeon_list[name][attribute] = [key.to_s]
+        end
+      else pigeon_list[name] = {attribute => [key.to_s]}
+        end
+        }
+      }
+    }
+pigeon_list# write your code here!
 end
+
+
